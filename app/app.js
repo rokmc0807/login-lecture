3ea4c54c14ac6159c2d379178b6370aa4b50a3ca
rokmc0807 <rokmc0807@navr.com>
2023-01-18 16:24:20 +0900

    폴더 구조 최적화

--- app/app.js
+++ app/app.js
@@ -6,11 +6,11 @@ const app = express();
 
 
 // 라우팅
-const home = require("./routes/home")
+const home = require("./src/routes/home")
 
 
 //앱 셋팅 파일이 저장될 위치 저장
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");      //html View 엔진 코드 정의
 
 // use 미들웨어 등록하는 메소드
