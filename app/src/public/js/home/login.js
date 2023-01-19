bf803ae740f484ce3c595c682043956bfebf1614
rokmc0807 <rokmc0807@navr.com>
2023-01-19 09:03:11 +0900

    돔을 이용하여 html 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,5 +1,21 @@
 "use scrict";
 
-console.log("hello");
-console.log("bye");
+const id = document.querySelector("#id"),
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("button");
+
+    loginBtn.addEventListener("click", login);
+
+    function login(){
+
+        const req = {
+            id:id.value,
+            psword:psword.value,
+        };
+
+        console.log(req)
+    }
+
+//console.log(id);
+//console.log(psword);
 
